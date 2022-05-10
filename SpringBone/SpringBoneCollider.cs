ac9784a2c461d097578f09f1cb0adca543f9e7d4
Jorgy <71365133+Uraraka-Chan@users.noreply.github.com>
2022-05-10 00:14:05 -0500

    Create SpringBoneCollider.cs

---
+++ SpringBone/SpringBoneCollider.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+	public class SpringCollider : MonoBehaviour
+	{
+		public float radius = 0.5f;
+
+		private void OnDrawGizmosSelected (){
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere (transform.position, radius);
+		}
+	}
+}
